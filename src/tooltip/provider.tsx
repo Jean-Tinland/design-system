941caef030046d362a7b0a2980289cd54cb6b21f
Jean Tinland <jean.tinland@gmail.com>
2022-09-02 20:44:02 +0200

    Prevent tooltip rerender on mouse over + bump 1.0.1

--- src/tooltip/provider.tsx
+++ src/tooltip/provider.tsx
@@ -16,43 +16,53 @@ const TooltipProvider = ({ children }: Props): JSX.Element => {
   const [anchor, setAnchor] = React.useState<Element | undefined>();
   const [tooltip, setTooltip] = React.useState<string | undefined>();
 
-  const show: EventListener = (e) => {
-    setUniqueId(getUniqueId());
-    const target = e.target as Element;
-    const element = target.closest(`[${ATTRIBUTE}]`);
-    if (element) {
-      const value = element.getAttribute(ATTRIBUTE);
-      if (value) {
-        setAnchor(element);
-        setTooltip(value);
+  const show: EventListener = React.useCallback(
+    (e) => {
+      const target = e.target as Element;
+      const element = target.closest(`[${ATTRIBUTE}]`);
+      if (element) {
+        const value = element.getAttribute(ATTRIBUTE);
+        if (value) {
+          setAnchor((previous) => {
+            if (previous !== element) {
+              setUniqueId(getUniqueId());
+            }
+            return element;
+          });
+          setTooltip(value);
+        }
       }
-    }
-  };
+    },
+    [anchor]
+  );
 
   const hide = () => {
     setTooltip(undefined);
   };
 
-  const hover: EventListener = (e) => {
-    requestAnimationFrame(() => {
-      const target = e.target as Element;
-      if (target) {
-        const hoveredTooltip = target.closest(`[${ATTRIBUTE}]`);
-        if (hoveredTooltip) {
-          show(e);
-        } else {
-          hide();
+  const hover: EventListener = React.useCallback(
+    (e) => {
+      requestAnimationFrame(() => {
+        const target = e.target as Element;
+        if (target) {
+          const hoveredTooltip = target.closest(`[${ATTRIBUTE}]`);
+          if (hoveredTooltip) {
+            show(e);
+          } else {
+            hide();
+          }
         }
-      }
-    });
-  };
+      });
+    },
+    [anchor]
+  );
 
   React.useEffect(() => {
     window.addEventListener("mouseover", hover);
     return () => {
       window.removeEventListener("mouseover", hover);
     };
-  }, []);
+  }, [hover]);
 
   return (
     <>
